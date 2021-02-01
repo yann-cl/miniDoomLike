ebf86a9b4f927301c3daef37aeaf88a814bdaf33
Fanthom117 <simon.ribeiro@yahoo.fr>
2021-02-01 15:39:09 +0100

    add better map

--- GameMap.cs
+++ GameMap.cs
@@ -14,10 +14,12 @@ namespace miniDoomLike{
                     this.cases[i,j]= Color.White;
                 }
             }
-            //mur de la map
+            //murs de la map
             contour(Color.Red);
+            murs(Color.Red);
+            
             //point d'entrée de la map
-            entryPoint = new Vector2D(2,20);
+            entryPoint = new Vector2D(100,50);
             cases[(int)entryPoint.x,(int)entryPoint.y] = Color.Orange;
         }
         //init le contour de la map de couleur différente
@@ -30,6 +32,44 @@ namespace miniDoomLike{
             for(int j=0;j<this.cases.GetLength(1);j++){
                 this.cases[0,j]= couleur;
                 this.cases[199,j]= couleur;
+            }
+        }
+        private void murs(Color couleur){
+            //milieu de la map
+            for(int i=0; i<this.cases.GetLength(0)/3; i++){
+                this.cases[i,this.cases.GetLength(0)/2]= couleur;
+                for(int j=(this.cases.GetLength(0)/3)*2; j<this.cases.GetLength(0); j++){
+                    this.cases[j,this.cases.GetLength(0)/2]= couleur;
+                }
+            }
+            //haut de la map
+            for(int i=this.cases.GetLength(0)/4;i<3*this.cases.GetLength(0)/8;i++){
+                this.cases[i,this.cases.GetLength(0)/8]= couleur;
+                for(int j=5*this.cases.GetLength(0)/8;j<3*this.cases.GetLength(0)/4;j++){
+                    this.cases[j,this.cases.GetLength(0)/8]= couleur;
+                }
+            }
+            for(int i=this.cases.GetLength(0)/4;i<3*this.cases.GetLength(0)/4;i++){
+                this.cases[i,this.cases.GetLength(0)/3]= couleur;
+            }
+            for(int j=this.cases.GetLength(0)/8;j<this.cases.GetLength(1)/3;j++){
+                this.cases[this.cases.GetLength(0)/4,j]= couleur;
+                this.cases[3*this.cases.GetLength(0)/4,j]= couleur;
+            }
+            //bas de la map
+            for(int i=this.cases.GetLength(0)/4;i<3*this.cases.GetLength(0)/4;i++){
+                this.cases[i,2*this.cases.GetLength(0)/3]= couleur;
+            }
+            for(int i=this.cases.GetLength(0)/4;i<3*this.cases.GetLength(0)/8;i++){
+                this.cases[i,7*this.cases.GetLength(0)/8]= couleur;
+                for(int j=5*this.cases.GetLength(0)/8;j<3*this.cases.GetLength(0)/4;j++){
+                    this.cases[j,7*this.cases.GetLength(0)/8]= couleur;
+                }
+            }
+            
+            for(int j=2*this.cases.GetLength(0)/3;j<7*this.cases.GetLength(1)/8;j++){
+                this.cases[this.cases.GetLength(0)/4,j]= couleur;
+                this.cases[3*this.cases.GetLength(0)/4,j]= couleur;
             }
         }
     }
